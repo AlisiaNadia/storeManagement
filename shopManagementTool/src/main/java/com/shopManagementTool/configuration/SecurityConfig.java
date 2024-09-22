c2d434491470330d5fa8ceec3e7e7575d60216ef
alisia <alisianadiasarb@gmail.com>
2024-09-23 00:12:06 +0300

    Fixed errors

--- shopManagementTool/src/main/java/com/shopManagementTool/configuration/SecurityConfig.java
+++ shopManagementTool/src/main/java/com/shopManagementTool/configuration/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().disable();
 //                .authorizeRequests()
 //                .requestMatchers("/public/**").permitAll()
 //                .requestMatchers("/admin/**").hasRole("ADMIN");
